e642f33da750fe221edf9d7e149c786fd56b56b5
Dan Dev <ifdanthencool@gmail.com>
2017-04-12 10:17:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,40 @@
-// quiz begins, no answers correct
-var correct = 0;
+	// quiz begins, no answers correct
+	var correct = 0;
+	
+	var answer1 = prompt("What programming language is a gem?");
+	if(answer1.toUpperCase() === 'RUBY') {
+		correct += 1;
+	}
+	
+	var answer2 = prompt("What programming language is a snake?");
+	if(answer2.toUpperCase() === 'PYTHON') {
+		correct +=1;	
+	}
+	
+	var answer3 = prompt("What language is used to style web pages?");
+	if(answer3.toUpperCase() === "CSS") {
+		correct += 1;
+	}
+	
+	var answer4 = prompt("What language makes up the structure of a web page?");
+	if(answer4.toUpperCase() === "HTML") {
+		correct += 1;
+	}
+	
+	var answer5 = prompt("What programming language is used to add interactiviy to a web page?");
+	if(answer5.toUpperCase() === "JAVASCRIPT"){
+		correct += 1;
+	}
+	// output results
+	document.write("<div class='container'><div class='jumbotron text-center'><h3>You got " + correct + " out of 5 questions correct!</h3>");
 
-// ask questions
-var answer1 = prompt("Name a programming language that's also a gem");
-if ( answer1.toUpperCase() === 'RUBY') {
-	correct += 1;
-}
-var answer2 = prompt("Name a programming language that's also a snake");
-if ( answer2.toUpperCase() === 'PYTHON') {
-	correct += 1;
-}
-var answer3 = prompt("What language do you use to style web pages?");
-if ( answer3.toUpperCase() === 'CSS') {
-	correct += 1;
-}
-var answer4 = prompt("What language do you use to build the structure of web pages?");
-if ( answer4.toUpperCase() === 'HTML') {
-	correct += 1;
-}
-var answer5 = prompt("What language do you use to add interactivity to a web page?");
-if ( answer5.toUpperCase() === 'JAVASCRIPT') {
-	correct += 1;
-}
-
-// output results
-document.write("<p>You got " + correct + " out of 5 questions correct.</p>");
-
-// output rank
-if ( correct === 5 ) {
-	document.write("<p><strong>You earned a gold crown!</strong></p>");
-} else if ( correct >= 3 ) {
-	document.write("<p><strong>You earned a silver crown!</strong></p>");
-} else if (correct >=1) {
-	document.write("<p><strong>You earned a bronze crown!</strong></p>");
-} else {
-	document.write("<p><strong>No crown for you. You need to study</strong></p>");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- && operator.  BOTH MUST BE TRUE
- There's no limit to the number of && operators you can string together
- Every single condition must be true for the && operator to work
- ( 20 < age && age < 30 )
-
- || operator. Either one may be true, but not both.
- You can string together multiple || operators
- ( password === "" || email === "" || phoneNumber === "") 
- In this case, only one of the conditions needs to be true
- ( agree === 'yes' || agree === 'y' )
-*/
+	// output rank
+	if(correct === 5){
+		document.write("<p><strong>You get a <span id='gold'>gold</span> crown!</strong></p>");
+	} else if (correct >=3){
+		document.write("<p><strong>You get a <span id='silver'>silver</span> crown!</strong></p>");
+	} else if (correct >=1){
+		document.write("<p><strong>You get a <span id='orange'>bronze</span> crown!</strong></p>");
+	} else {
+		document.write("<p><strong>No crown for you. You need to study more.</strong></p></div></div>");
+	}
